b8b8ad5a3912be95c1f4406341cbbe706641f54b
Shweta <shwetasha03@gmail.com>
2020-09-02 20:34:00 +0530

    first commit

---
+++ frontend/src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './app.css';
+import Details from './components/details';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return <Details />;
+}
+
+export default App;
+
